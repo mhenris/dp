0def1fe20239dbaedba3b4bb4bb5d294aa57eec5
Morgan Henris <morganhenris@gmail.com>
2012-02-13 22:47:00 -0500

    Just adding some base stuff

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe PagesController do
+
+  render_views
+
+	before(:each) do
+		@base_title = "Daddy Paradise"
+	end
+
+  describe "GET 'home'" do
+    it "returns http success" do
+      get 'home'
+      response.should be_success
+    end
+		it "should have the right title" do
+			get 'home'
+				response.should have_selector("title",
+					:content => "Home | " + @base_title)
+		end
+  end
+
+end
